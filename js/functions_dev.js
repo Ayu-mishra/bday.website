6dd46b594c98cc046a6f8a5c70a41e0f25297a67
Ayu-mishra <66792153+Ayu-mishra@users.noreply.github.com>
2020-06-11 21:33:02 +0530

    Update functions_dev.js

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -117,7 +117,7 @@ function timeElapse(date){
 
 function showMessages() {
 	adjustWordsPosition();
-	$('#messages').fadeIn(5000, function() {
+	$('#messages').fadeIn(10000, function() {
 		showLoveU();
 	});
 }
@@ -133,5 +133,5 @@ function adjustCodePosition() {
 }
 
 function showLoveU() {
-	$('#loveu').fadeIn(3000);
+	$('#loveu').fadeIn(8000);
 }
